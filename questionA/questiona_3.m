52d4a007a1b90c1265bc1f6fbb7563aadb43a553
MINWU-beginner <72280721+MINWU-beginner@users.noreply.github.com>
2020-11-16 02:10:21 +0000

    Add files via upload

---
+++ questionA/questiona_3.m
@@ -0,0 +1,44 @@
+
+close all
+clear all
+clc
+%// Read in image - make sure we cast to double
+img1 = imread('zebra.jpg');
+img2 = rgb2gray(img1)
+B = double(img2);
+%// Calculate covariance matrix
+sigma = cov(B);
+
+%// Find eigenvalues and eigenvectors of the covariance matrix
+[A,D] = eig(sigma);
+vals = diag(D);
+
+%// Sort their eigenvalues
+[~,ind] = sort(abs(vals), 'descend');
+
+%// Rearrange eigenvectors
+Asort = A(:,ind);
+
+%// Find mean subtracted data
+Bm = bsxfun(@minus, B, mean(B,1));
+
+%// Reproject data onto principal components
+Bproject = Bm*Asort;
+
+%%%%%%%// Begin reconstruction logic
+figure;
+counter = 1;
+
+for k = [30 50 80 90 100 110 120 130 140 150 160 170 180 190 200]
+    %// Extract out highest k eigenvectors
+    Aq = Asort(:,1:k);
+
+    %// Project back onto original domain
+    out = bsxfun(@plus, Bproject(:,1:k)*Aq.', mean(B, 1));
+
+    %// Place projection onto right slot and show the image
+    subplot(3, 5, counter);
+    counter = counter + 1;
+    imshow(out,[]);
+    title(['k = ' num2str(k)]);
+end
